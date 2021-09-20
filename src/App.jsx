c5bfc6d62d7bc942c412a3168b8c95596cf9a558
hernangit config --global user.name John <hernan0066@gmail.com>
2021-09-20 14:15:03 -0300

    1.7.8

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
-import { collection, getDocs, addDoc } from "firebase/firestore";
+import { collection, getDocs, addDoc, doc, deleteDoc } from "firebase/firestore";
 import db from './firebase';
 
 function App() {
@@ -48,16 +48,38 @@ function App() {
     
     console.log(nuevaTarea)
   }
+
+  const eliminar = async (id)=>{
+    try {
+      await deleteDoc(doc(db, "tareas", id));
+      
+      const arrayFiltrado = tareas.filter(item=> item.id !== id);
+      setTareas(arrayFiltrado);
+
+
+    } catch (error) {
+      console.log(error)
+      
+    }
+  }
   
   return (
     <div className="container mt-3">
       <div className="row">
         <div className="col-md-6">
+        <h3>Lista de tareas</h3>
           <ul className="list-group">
             {
               tareas.map(item=>(
                 <li className="list-group-item" key={item.id}>
                   {item.name}
+                
+                    <button 
+                        className="btn btn-danger btn-sm float-end"
+                        onClick={()=> eliminar(item.id)}
+                    >Eliminar
+                    </button>
+                    <button className="btn btn-danger btn-sm float-end mx-3">Editar</button>
                 </li>
               ))
             }
